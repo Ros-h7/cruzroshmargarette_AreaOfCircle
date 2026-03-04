a06a48cb27c2f0a6bd40dd7043095175458bccdd
Ros-h7 <cruzmargarette3@gmail.com>
2026-03-04 10:14:05 +0800

    Add files via upload

---
+++ CruzRoshMargarette_AreaOfCircle/app.py
@@ -0,0 +1,9 @@
+import math
+
+print("------------------------------")
+print("      AREA OF A CIRCLE ")
+print("------------------------------")
+CruzRadius = float(input("Enter the radius: "))
+CruzArea = math.pi * (CruzRadius ** 2)
+print(f"The approximate area is: {(CruzArea)}")
+
